35c97e6dff7192c7c90eda814b270748400edfd2
jadynbb <jadynbb@gmail.com>
2024-11-29 20:20:36 -0800

    Edit Summary
    I added a title screen under the MainMenuScene.js file.  I also moved the serval preloads from scene1 to its own file to make the scene1 file look cleaner. After making these two new files, I declared them in the HTML file.

---
+++ LoaderScene.js
@@ -0,0 +1,57 @@
+class LoaderScene extends Phaser.Scene {
+    constructor() {
+      super("loader-scene");
+    }
+
+preload(){
+    this.load.image("starbackground", "assets/images/starbackground.jpeg");
+    //
+    this.load.spritesheet("ship", "assets/spritesheets/ship.png",{
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("ship2", "assets/spritesheets/ship2.png",{
+      frameWidth: 32,
+      frameHeight: 16
+    });
+    this.load.spritesheet("ship3", "assets/spritesheets/ship3.png",{
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.spritesheet("explosion", "assets/spritesheets/explosion.png",{
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("power-up", "assets/spritesheets/power-up.png",{
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("player", "assets/spritesheets/player.png",{
+      frameWidth: 16,
+      frameHeight: 24
+    });
+    this.load.spritesheet("beam", "assets/spritesheets/beam.png",{
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("smallasteroid", "assets/spritesheets/smallasteroid.png",{
+      frameWidth: 20,
+      frameHeight: 20
+    });
+    this.load.spritesheet("medasteroid", "assets/spritesheets/medasteroid.png",{
+      frameWidth: 39,
+      frameHeight: 39
+    });
+
+    this.load.bitmapFont("pixelFont", "assets/font/font.png", "assets/font/font.xml");
+
+    this.load.audio("audio_beam", ["assets/sounds/beam.ogg", "assets/sounds/beam.mp3"]);
+    this.load.audio("audio_explosion", ["assets/sounds/explosion.ogg", "assets/sounds/explosion.mp3"]);
+    this.load.audio("audio_pickup", ["assets/sounds/pickup.ogg", "assets/sounds/pickup.mp3"]);
+    this.load.audio("music", ["assets/sounds/sci-fi_platformer12.ogg", "assets/sounds/sci-fi_platformer12.mp3"]);
+  }
+
+  create() {
+    this.scene.switch("main-menu-scene")
+  }
+}
--- game.js
+++ game.js
@@ -8,7 +8,7 @@ var config = {
   width: 800,
   height: 600,
   backgroundColor: 0x000000,
-  scene: [Scene1, Scene2],
+  scene: [LoaderScene, MainMenuScene, Scene1, Scene2],
   pixelArt: true,
   physics: {
     default: "arcade",
